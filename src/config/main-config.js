fe13b07d5ddd832bd927774b094f11876642757f
Brandon Kim <bkim2490@gmail.com>
2019-01-15 21:47:40 +0900

    moved if process statement from main-config to server

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -6,14 +6,5 @@ module.exports = {
     init(app, express) {
         app.use(bodyParser.json());
         app.use(bodyParser.urlencoded({ extended: true }));
-        // if (process.env.NODE_ENV === 'production') {
-            // Serve any static files
-            app.use(express.static(path.join(__dirname, 'client/build')));
-
-            // Handle React routing, return all requests to React app
-            app.get('*', function(req, res) {
-                res.sendFile(path.join(__dirname, 'client/build/index.html'));
-            });
-        // }
     }
 }
--- src/server.js
+++ src/server.js
@@ -19,6 +19,16 @@ function normalizePort(val) {
     return false;
 }
 
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+
+    // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+        res.sendFile(path.join(__dirname, 'client/build/index.html'));
+    });
+}
+
 server.on('listening', () => {
     console.log(`server is listening for requests on port ${server.address().port}`);
 })
