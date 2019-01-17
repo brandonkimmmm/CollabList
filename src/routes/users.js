235813b04059c378bbe557dd21bd6428890bbf43
Brandon Kim <bkim2490@gmail.com>
2019-01-17 18:35:17 +0900

    changed signup url to api/users

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
-router.post('/users', userController.create);
+router.post('/api/users', userController.create);
 
 module.exports = router;
