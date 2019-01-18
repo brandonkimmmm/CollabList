40f506621beb11af74df4bfa006b28c862a5c46b
Brandon Kim <bkim2490@gmail.com>
2019-01-18 18:07:59 +0900

    signin sets this.state.user to new user or sends failed message if credentials are wrong

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,8 +3,12 @@ const passport = require('passport');
 
 module.exports = {
     create(req, res, next) {
+        let response = {
+            'user': undefined
+        };
         if(req.body.password !== req.body.passwordConfirmation) {
-            res.send('Error: password confirmation must match password');
+            response.message = 'Error: password confirmation must match password';
+            res.send(response);
         } else {
             let newUser = {
                 email: req.body.email,
@@ -13,12 +17,13 @@ module.exports = {
             }
             userQueries.createUser(newUser, (err, user) => {
                 if(err) {
-                    console.log(req.isAuthenticated());
-                    res.send('Error: ' + err.errors[0].message);
+                    response.message = 'Error: ' + err.errors[0].message;
+                    res.send(response);
                 } else {
                     passport.authenticate('local')(req, res, () => {
-                        console.log(req.isAuthenticated());
-                        res.send('Account Created!');
+                        response.message = 'Account Created!';
+                        response.user = req.user;
+                        res.send(response);
                     });
                 }
             });
@@ -26,21 +31,46 @@ module.exports = {
     },
 
     signin(req, res, next) {
-        passport.authenticate('local')(req, res, function() {
-            if(!req.user) {
-                res.send('Error: wrong credentials');
-            } else {
-                res.send('Successfully signed in!');
+        let response = {
+            'user': undefined
+        }
+        // passport.authenticate('local')(req, res, function() {
+        //     if(!req.user) {
+        //         response.message = 'Error: wrong credentials';
+        //         return res.send(response);
+        //     } else {
+        //         response.message = 'Successfully signed in!';
+        //         response.user = req.user;
+        //         res.send(response);
+        //     }
+        // })
+
+        passport.authenticate('local', function(err, user, info) {
+            if (err || !user) {
+                response.message = 'Error: wrong credentials';
+                return res.send(response);
             }
-        })
+            req.logIn(user, function(err) {
+                if (err) {
+                    response.message = 'Error: wrong credentials';
+                    return res.send(response);
+                }
+                response.message = 'Successfully signed in!';
+                response.user = req.user;
+                return res.send(response);
+            });
+        })(req, res, next);
     },
 
     isAuth(req, res, next) {
+        let response = {
+            'user': undefined
+        }
         if(!req.isAuthenticated()) {
-            res.send(false);
+            res.send(response);
         } else {
-            console.log(req.user.email);
-            res.send(req.user);
+            response.user = req.user;
+            res.send(response);
         }
     },
 
